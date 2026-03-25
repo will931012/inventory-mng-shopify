183cfd84d6cfe14b7a931e59908d5439c2e5f935
will931012 <wilianpg93@gmail.com>
2026-03-25 10:02:38 -0400

    Adapt Remix entry for Vercel runtime

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,103 +1,16 @@
-import { PassThrough } from "node:stream";
-
-import { createReadableStreamFromReadable } from "@remix-run/node";
+import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import { handleRequest as vercelHandleRequest } from "@vercel/remix";
 
 import { addDocumentResponseHeaders } from "./shopify.server";
 
-const ABORT_DELAY = 5_000;
-
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: Parameters<typeof RemixServer>[0]["context"]
+  remixContext: EntryContext
 ) {
-  return isbot(request.headers.get("user-agent"))
-    ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
-    : handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext);
-}
-
-function handleBotRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: Parameters<typeof RemixServer>[0]["context"]
-) {
-  return new Promise<Response>((resolve, reject) => {
-    let didError = false;
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
-      {
-        onAllReady() {
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
-          addDocumentResponseHeaders(request, responseHeaders);
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode
-            })
-          );
-
-          pipe(body);
-        },
-        onShellError(error) {
-          reject(error);
-        },
-        onError(error) {
-          didError = true;
-          console.error(error);
-        }
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
-  });
-}
-
-function handleBrowserRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: Parameters<typeof RemixServer>[0]["context"]
-) {
-  return new Promise<Response>((resolve, reject) => {
-    let didError = false;
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
-      {
-        onShellReady() {
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
-          addDocumentResponseHeaders(request, responseHeaders);
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode
-            })
-          );
-
-          pipe(body);
-        },
-        onShellError(error) {
-          reject(error);
-        },
-        onError(error) {
-          didError = true;
-          console.error(error);
-        }
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
-  });
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+  addDocumentResponseHeaders(request, responseHeaders);
+  return vercelHandleRequest(request, responseStatusCode, responseHeaders, remixServer);
 }
